4a346c3ff88fd00cb76ec0a096afbd3d2ea2cceb
Samuele Medici <samuele.medici@quinck.io>
2024-12-02 22:28:36 +0100

    feat: :sparkles: complete day 2
    minor refactor on day 2 in order to work properly

--- lib/solution_day2.ex
+++ lib/solution_day2.ex
@@ -5,39 +5,61 @@ defmodule SolutionDay2 do
     Enum.sort(list) == list || Enum.reverse(Enum.sort(list)) == list
   end
 
-  defp check_next(_current, []) do
+  defp check_increment(_current, []) do
     true
   end
 
-  defp check_next(current, [next | rest]) do
+  defp check_increment(current, [next | rest]) do
     diff = abs(current - next)
 
     if(diff >= 1 && diff <= 3) do
-      check_next(next, rest)
+      check_increment(next, rest)
     else
       false
     end
   end
 
   def is_gradually_incrementing([head | tail]) do
-    check_next(head, tail)
+    check_increment(head, tail)
+  end
+
+  defp parse_reports(content) do
+    content
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn report ->
+      report
+      |> String.split(" ", trim: true)
+      |> Enum.map(fn x -> String.to_integer(x) end)
+    end)
+  end
+
+  defp is_report_safe_part_1(list) do
+    is_increasing_or_decreasing_only(list) and is_gradually_incrementing(list)
+  end
+
+  defp is_report_safe_part_2(list) do
+    is_report_safe_part_1(list) or
+      list
+      |> Enum.with_index()
+      |> Enum.any?(fn {_element, index} -> is_report_safe_part_1(List.delete_at(list, index)) end)
   end
 
   def solve do
-    {:ok, content} = File.read("data/input_day2.txt")
+    {:ok, content} = FileReader.parse_input("data/input_day2.txt")
 
-    safe_report =
+    part_1_safe_report =
       content
-      |> String.split("\n", trim: true)
-      |> Enum.map(fn report ->
-        report
-        |> String.split(" ", trim: true)
-        |> Enum.map(fn x -> String.to_integer(x) end)
-      end)
-      |> Enum.filter(&is_increasing_or_decreasing_only/1)
-      |> Enum.filter(&is_gradually_incrementing/1)
+      |> parse_reports()
+      |> Enum.filter(&is_report_safe_part_1/1)
       |> Enum.count()
 
-    IO.inspect(safe_report)
+    part_2_safe_report =
+      content
+      |> parse_reports()
+      |> Enum.filter(&is_report_safe_part_2/1)
+      |> Enum.count()
+
+    IO.puts(part_1_safe_report)
+    IO.puts(part_2_safe_report)
   end
 end
