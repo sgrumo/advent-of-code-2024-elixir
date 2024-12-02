2854383626138f541e6e2b98b436d118677d5d8e
Samuele Medici <samuele.medici@quinck.io>
2024-12-02 15:27:04 +0100

    feat: :sparkles: complete day 1

--- lib/solution_day1.ex
+++ lib/solution_day1.ex
@@ -4,7 +4,7 @@ defmodule SolutionDay1 do
   def solve do
     {:ok, content} = FileReader.parse_input("data/input_day1.txt")
 
-    values =
+    [first_list, second_list] =
       content
       |> String.split("\n", trim: true)
       |> Enum.flat_map(fn s ->
@@ -15,18 +15,31 @@ defmodule SolutionDay1 do
       |> Enum.with_index()
       |> Enum.split_with(fn {_k, v} -> Integer.is_even(v) end)
       |> Tuple.to_list()
-
-    distance =
-      values
       |> Enum.map(fn list ->
         list
         |> Enum.map(fn {{value, _s}, _index} -> value end)
         |> Enum.sort()
       end)
+
+    total_distance =
+      [first_list, second_list]
       |> Enum.zip_reduce(0, fn [first, second], acc ->
         acc + abs(first - second)
       end)
 
-    IO.puts(distance)
+    total_similarity =
+      first_list
+      |> Enum.map(fn item ->
+        count =
+          second_list
+          |> Enum.filter(fn x -> x == item end)
+          |> Enum.count()
+
+        count * item
+      end)
+      |> Enum.sum()
+
+    IO.puts(total_distance)
+    IO.puts(total_similarity)
   end
 end
